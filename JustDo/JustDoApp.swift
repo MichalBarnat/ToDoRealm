d3c5facfaabefc75e7beb09a04bbbe866c236fab
Michal Barnat <michalbarnat@outlook.com>
2022-08-08 13:37:29 +0200

    Initial Commit

---
+++ JustDo/JustDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustDoApp.swift
+//  JustDo
+//
+//  Created by Michał Barnat on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct JustDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
